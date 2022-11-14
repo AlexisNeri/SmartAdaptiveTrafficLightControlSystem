11ba14aeecf15ae414490ced33688b25e71179fc
Alexis Romo <aljorone93@gmail.com>
2022-11-13 20:50:42 -0600

    Resolve pending TODOs and added __pycache__ folder to .gitignore file.

--- car_counter.py
+++ car_counter.py
@@ -5,8 +5,10 @@ from cvlib.object_detection import draw_bbox
 
 def car_counter(frame, target_labels, debug_log=False):
     object_count = 0
-    # TODO: Make frame resizing dynamic to 1/2 of input
-    frame = imutils.resize(frame, width=960, height=540)
+    if (frame.shape[1] % 2 == 0) and (frame.shape[0] % 2 == 0):
+        image_width = int(frame.shape[1]/2)
+        image_height = int(frame.shape[0]/2)
+        frame = imutils.resize(frame, width=image_width, height=image_height)
     bbox, label, conf = cv.detect_common_objects(frame, confidence=0.25, model='yolov4-tiny')
     if debug_log:
         print(bbox, label, conf)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import car_counter
 import decision_maker
 import cv2
+import math
 
 
 def main_module(main_street_vsource, secondary_street_vsource, show_output=False, show_logs=False,
@@ -9,14 +10,18 @@ def main_module(main_street_vsource, secondary_street_vsource, show_output=False
     counter = 0
     main_road = 0
     main_vsource = cv2.VideoCapture(main_street_vsource)
-    main_vsource_fps = int(main_vsource.get(cv2.CAP_PROP_FPS))
+    main_vsource_fps = math.ceil(main_vsource.get(cv2.CAP_PROP_FPS))
     print('Frame rate for main video source is: {}'.format(main_vsource_fps))
     secondary_vsource = cv2.VideoCapture(secondary_street_vsource)
-    secondary_vsource_fps = int(secondary_vsource.get(cv2.CAP_PROP_FPS))
+    secondary_vsource_fps = math.ceil(secondary_vsource.get(cv2.CAP_PROP_FPS))
     print('Frame rate for secondary video source is: {}'.format(secondary_vsource_fps))
 
+    if main_vsource_fps != secondary_vsource_fps:
+        print('Error: video sources have different frame rates')
+        exit()
+
     if not main_vsource.isOpened() or not secondary_vsource.isOpened():
-        print("Error: could not open video sources")
+        print('Error: could not open video sources')
         exit()
 
     while main_vsource.isOpened() and secondary_vsource.isOpened():
@@ -25,8 +30,7 @@ def main_module(main_street_vsource, secondary_street_vsource, show_output=False
         if not main_status or not secondary_status:
             break
         # Process 1 frame per second
-        # TODO: Make this process dynamic based on frame rate
-        if counter == 30:
+        if counter == main_vsource_fps/2:
             street_1_count, street_1_bbox = car_counter.car_counter(main_frame,
                                                                     ['motorcycle', 'car', 'truck', 'bus', 'cell phone'],
                                                                     debug_log=show_logs)
@@ -37,11 +41,11 @@ def main_module(main_street_vsource, secondary_street_vsource, show_output=False
             if show_count:
                 print('Vehicles at street #1: {0}\n Vehicles at street #2: {1}'.format(street_1_count, street_2_count))
             if show_output:
-                cv2.imshow("Street #1", street_1_bbox)
-                cv2.imshow("Street #2", street_2_bbox)
+                cv2.imshow('Street #1', street_1_bbox)
+                cv2.imshow('Street #2', street_2_bbox)
 
         counter += 1
-        if counter == 60:
+        if counter == main_vsource_fps:
             counter = 0
             time += 1
 
@@ -54,4 +58,5 @@ def main_module(main_street_vsource, secondary_street_vsource, show_output=False
 
 
 if __name__ == '__main__':
+    # TODO: Change video routes from hardcoded to dynamic
     main_module('Video/DJI_0054.MP4', 'Video/DJI_0055.MP4', show_output=True, show_count=True)
